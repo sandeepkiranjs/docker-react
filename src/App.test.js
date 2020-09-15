0cb5b1766fc32df14eaaa90155ae2ded5f23e126
sandeepkiranjs22 <sandeep.kiran@bankbuddy.ai>
2020-09-15 12:44:46 +0530

    Test suite updated

--- src/App.test.js
+++ src/App.test.js
@@ -4,10 +4,10 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App/>, div);
 });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App/>, div);
 });
